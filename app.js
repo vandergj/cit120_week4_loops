364b4ad56d8fd3842ff6b1e773f0380505c02620
vandergj <vandergj@iu.edu>
2022-02-01 18:28:31 -0500

    lecture

---
+++ app.js
@@ -0,0 +1,20 @@
+let num=0;
+
+//for(let i=10; i>0; i--){
+//    num=num+2;
+//}
+
+// let start=0;
+// while(start<5){
+//     num=num+3;
+//     start++;
+// }
+
+let ans='';
+while(ans!=="*"){
+    ans=prompt("What symbol would you like?")
+}
+
+
+
+document.body.innerHTML=num;
