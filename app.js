674fdb93fe219c1018b7f75174f69770119a6630
vandergj <89868874+vandergj@users.noreply.github.com>
2022-02-08 18:07:47 -0500

    remove console.log statements

--- app.js
+++ app.js
@@ -14,20 +14,17 @@ if (usr_hits > 5){
 output = "Please enter a number from 1-5"
 do_loop = false;
 }else {
-    console.log("Made it to damage")
     usr_attack = Math.floor((Math.random() * usr_hits) + 1);
     dragon_attack = Math.floor((Math.random() * 2) + 1);
     dragon_hp = dragon_hp - usr_attack;
     user_hp = user_hp - dragon_attack;
     if (dragon_hp < 1){
-        console.log("User Won")
         do_loop = false;
         dragon_total_damage = 5 - user_hp;
         usr_total_damage = 10 - dragon_hp;
         output = "User Damage: " + usr_total_damage.toString() + "<br>" + "User Remaining Hp: " + user_hp.toString() + "<br>" + "Dragon Damage: " + dragon_total_damage.toString() + "<br>" + "Dragon Remaining Hp: " + dragon_hp.toString() + "<br>" + "User Wins"
     }else if (user_hp < 1){
         do_loop = false;
-        console.log("Dragon Won")
         dragon_total_damage = 5 - user_hp;
         usr_total_damage = 10 - dragon_hp;
         output = "User Damage: " + usr_total_damage.toString() + "<br>" + "User Remaining Hp: " + user_hp.toString() + "<br>" + "Dragon Damage: " + dragon_total_damage.toString() + "<br>" + "Dragon Remaining Hp: " + dragon_hp.toString() + "<br>" + "User Loses"
@@ -35,5 +32,4 @@ do_loop = false;
 }
 }
 
-console.log("Output time")
 document.body.innerHTML=output;
